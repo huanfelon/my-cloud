12d27c032e3e84729f38157b9b9f33ba31da90de
皇飞龙 <2291267989@qq.com>
2023-02-14 09:18:14 +0800

    展示hystrix实例监控信息的仪表盘

---
+++ hystrix-dashboard/src/main/java/com/example/cloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.example.cloud.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableHystrixDashboard 启用监控功能
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
