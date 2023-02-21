2133955b0db3818b1eceec4521d295b01bc8339e
皇飞龙 <2291267989@qq.com>
2023-02-21 09:23:41 +0800

    nacos-config-client -- 用于演示nacos作为配置中心的nacos客户端 nacos-user-service -- 注册到nacos的提供User对象CRUD接口的服务 nacos-ribbon-service -- 注册到nacos的ribbon服务调用测试服务

---
+++ nacos-user-service/src/main/java/com/example/cloud/nacosuserservice/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.example.cloud.nacosuserservice.controller;
+
+import com.example.cloud.nacosuserservice.domain.CommonResult;
+import com.example.cloud.nacosuserservice.domain.User;
+import com.example.cloud.nacosuserservice.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    IUserService userService;
+
+    @GetMapping("/list")
+    public CommonResult<List<User>> getUserList() {
+        List<User> user = userService.getUserList();
+        log.info("获取用户所有信息");
+        return new CommonResult<>(user);
+    }
+
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody User user) {
+        userService.create(user);
+        return new CommonResult("操作成功", 200);
+    }
+
+    @GetMapping("/{id}")
+    public CommonResult<User> getUser(@PathVariable Long id) {
+        User user = userService.getUser(id);
+        log.info("根据id获取用户信息，用户名称为：{}",user.getUsername());
+        return new CommonResult<>(user);
+    }
+
+    @GetMapping("/getUserByIds")
+    public CommonResult<List<User>> getUserByIds(@RequestParam List<Long> ids) {
+        List<User> userList= userService.getUserByIds(ids);
+        log.info("根据ids获取用户信息，用户列表为：{}",userList);
+        return new CommonResult<>(userList);
+    }
+
+    @GetMapping("/getByUsername")
+    public CommonResult<User> getByUsername(@RequestParam String username) {
+        User user = userService.getByUsername(username);
+        return new CommonResult<>(user);
+    }
+
+    @PostMapping("/update")
+    public CommonResult update(@RequestBody User user) {
+        userService.update(user);
+        return new CommonResult("操作成功", 200);
+    }
+
+    @PostMapping("/delete/{id}")
+    public CommonResult delete(@PathVariable Long id) {
+        userService.delete(id);
+        return new CommonResult("操作成功", 200);
+    }
+}
